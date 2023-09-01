6c4f65f13f5e068e44466516494af68ef4997479
Jonathan Yong <yongrenjie@gmail.com>
2023-09-01 17:25:17 +0100

    'initial' checkpoint for blog post

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,71 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    let novels = [
+        {
+            title: "Emma",
+            opening:
+                "Emma Woodhouse, handsome, clever, and rich, with a comfortable home and happy disposition, seemed to unite some of the best blessings of existence; and had lived nearly twenty-one years in the world with very little to distress or vex her.",
+        },
+        {
+            title: "The Great Gatsby",
+            opening:
+                "In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since.",
+        },
+        {
+            title: "Mrs Dalloway",
+            opening: "Mrs Dalloway said she would buy the flowers herself.",
+        },
+        {
+            title: "The Catcher in the Rye",
+            opening:
+                "If you really want to hear about it, the first thing you'll probably want to know is where I was born, and what my lousy childhood was like, and how my parents were occupied and all before they had me, and all that David Copperfield kind of crap, but I don't feel like going into it, if you want to know the truth.",
+        },
+    ];
+
+    let index = 0;
+    function nextNovel() {
+        index = index + 1;
+    }
+    function previousNovel() {
+        index = index - 1;
+    }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div id="middle">
+        <div id="controls">
+            <button on:click={previousNovel} disabled={index === 0}>←</button>
+            <select bind:value={index}>
+                {#each novels as novel, i}
+                    <option value={i}>{novel.title}</option>
+                {/each}
+            </select>
+            <button on:click={nextNovel} disabled={index === novels.length - 1}
+                >→</button
+            >
+        </div>
+        <p id="opening">{novels[index].opening}</p>
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    main {
+        display: flex;
+        justify-content: center;
+        background-color: #ffe6f4;
+        padding: 20px;
+    }
+    div#middle {
+        background-color: #fcbaff;
+        border: 2px solid #e688eb;
+        padding: 10px;
+        width: 300px;
+    }
+    div#controls {
+        display: flex;
+        justify-content: space-between;
+        align-items: baseline;
+    }
+    p#opening {
+        margin-bottom: 0;
+    }
 </style>
